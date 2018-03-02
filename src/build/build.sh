6277903a3ea3fbc5ba838f2ba2508013eb9ab070
Олег <cronfy@gmail.com>
2018-03-02 18:14:01 +0300

    start after build

--- src/build/build.sh
+++ src/build/build.sh
@@ -4,8 +4,9 @@ cd `dirname $0` ; cd .. ; RDIR="`pwd`" ; . "$RDIR/init.sh"
 
 if [[ "$(docker images -q image-$PROJECT_NAME 2> /dev/null)" == "" ]]; then
     cp $RDIR/build/Dockerfile $LOCAL_DOCKER/build/
-    docker build -t image-$PROJECT_NAME --build-arg MYSQL_USER_PASS="$MYSQL_PASSWORD" $LOCAL_DOCKER/build
-    $RDIR/build/scripts/first-run.sh
+    docker build -t image-$PROJECT_NAME --build-arg MYSQL_USER_PASS="$MYSQL_PASSWORD" $LOCAL_DOCKER/build &&
+    $RDIR/build/scripts/first-run.sh &&
+    $RDIR/../run/start.sh
 else
     echo "ERROR: Image already exists, please run destroy.sh if you want to start a new build." >&2
     exit 1
