86109377b0a9411a956dfbbf45558fcfa65940f2
Олег <cronfy@gmail.com>
2018-03-02 18:15:57 +0300

    fix

--- src/build/build.sh
+++ src/build/build.sh
@@ -6,7 +6,7 @@ if [[ "$(docker images -q image-$PROJECT_NAME 2> /dev/null)" == "" ]]; then
     cp $RDIR/build/Dockerfile $LOCAL_DOCKER/build/
     docker build -t image-$PROJECT_NAME --build-arg MYSQL_USER_PASS="$MYSQL_PASSWORD" $LOCAL_DOCKER/build &&
     $RDIR/build/scripts/first-run.sh &&
-    $RDIR/../run/start.sh
+    $RDIR/run/start.sh
 else
     echo "ERROR: Image already exists, please run destroy.sh if you want to start a new build." >&2
     exit 1
